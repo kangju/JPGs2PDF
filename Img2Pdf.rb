e14f81ccec66043660962a552557b7fe23d118ee
kangju <kangju0815@yahoo.co.jp>
2015-05-19 00:41:35 +0900

    create Jpgs2Pdf Class

---
+++ Img2Pdf.rb
@@ -0,0 +1,26 @@
+require 'prawn'
+
+module Jpgs2Pdf
+   class Jpgs2Pdf
+      def Jpgs2Pdf.toPdf(targetFolderName,outputPdfFilePath)
+         searchtext = "#{targetFolderName}/*.jpg"
+         firstpage = true
+
+         imgs = Dir.glob(searchtext).sort!
+
+         return false if imgs.empty?
+
+         Prawn::Document.generate(outputPdfFilePath) do
+            imgs.each do |img|
+               start_new_page unless firstpage
+               image(img,
+                :at => [-1*bounds.absolute_left, bounds.absolute_top],
+                :fit => [bounds.absolute_right+bounds.absolute_left, bounds.absolute_top+bounds.absolute_bottom])
+               firstpage = false
+            end
+         end
+
+         return true
+      end
+   end
+end
